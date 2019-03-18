33154125ef470dc38b64eb165d52c38f0c1188da
xiwen.yu <xiwen.yu@jiangrongxin.com>
2019-03-18 17:19:34 +0800

    于西文的Java

---
+++ src/com/jrx/TestEqual.java
@@ -0,0 +1,7 @@
+package com.jrx;
+
+/**
+ * 用于练习equal
+ */
+public class TestEqual {
+}
---
+++ src/com/jrx/TestLambda.java
@@ -0,0 +1,14 @@
+package com.jrx;
+
+/**
+ * 子类用于lambda特性练习
+ */
+public class TestLambda  {
+    public  static  void  main (String args[]){
+
+
+
+
+    }
+
+}
